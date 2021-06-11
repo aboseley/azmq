bec12e3348f926261c83c497f5b43d63e9d0a231
Adam Boseley <adam.boseley@gmail.com>
2021-06-12 08:31:44 +1000

    Add zmq-guide book examples

---
+++ test/socket/request_reply_combinations.cpp
@@ -0,0 +1,243 @@
+/*
+    Copyright (c) 2013-2014 Contributors as noted in the AUTHORS file
+
+    This file is part of azmq
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#include <azmq/socket.hpp>
+#include <boost/asio/buffer.hpp>
+#include <catch2/catch.hpp>
+#include <array>
+#include <string>
+
+namespace {
+
+std::string subj(const char *name) {
+  return std::string("inproc://") + name;
+}
+
+std::array<boost::asio::const_buffer, 2> const snd_bufs = {{
+                                                               boost::asio::buffer("A"),
+                                                               boost::asio::buffer("B")
+                                                           }};
+}
+
+TEST_CASE("REQ to REP combination, synchronous", "[socket]") {
+  boost::asio::io_context ioc{};
+
+  auto sb = azmq::rep_socket{ioc};
+  sb.bind(subj(BOOST_CURRENT_FUNCTION));
+
+  auto sc = azmq::req_socket{ioc};
+  sc.connect(subj(BOOST_CURRENT_FUNCTION));
+
+// request
+  sc.send(snd_bufs);
+  auto received_request = azmq::message_vector{};
+  sb.receive_more(received_request, 0);
+  REQUIRE(received_request.size() == 2);
+
+// reply
+  sb.send(snd_bufs);
+  auto received_reply = azmq::message_vector{};
+  sc.receive_more(received_reply, 0);
+  REQUIRE(received_reply.size() == 2);
+}
+
+TEST_CASE("REQ to REP combination, asynchronous", "[socket]") {
+  boost::asio::io_context ioc{};
+  auto sb = azmq::rep_socket{ioc};
+  auto sc = azmq::req_socket{ioc};
+  bool caught_request = false;
+  bool caught_reply = false;
+
+  sb.bind(subj(BOOST_CURRENT_FUNCTION));
+  sc.connect(subj(BOOST_CURRENT_FUNCTION));
+  // catch the request
+  sb.async_receive(
+      [&](boost::system::error_code ec,
+          azmq::message &first_frame,
+          size_t bytes_transferred) {
+        if (!ec) {
+          auto message = azmq::message_vector{};
+          sb.receive_more(message, ZMQ_DONTWAIT);
+          REQUIRE(message.size() == 1);
+          REQUIRE(first_frame == snd_bufs.at(0));
+          REQUIRE(message.at(0) == snd_bufs.at(1));
+          caught_request = true;
+
+          // catch the reply
+          sc.async_receive(
+              [&](boost::system::error_code ec,
+                  azmq::message &first_frame,
+                  size_t bytes_transferred) {
+                if (!ec) {
+                  auto received_reply = azmq::message_vector{};
+                  sc.receive_more(received_reply, ZMQ_DONTWAIT);
+                  REQUIRE(received_reply.size() == 1);
+                  caught_reply = true;
+                }
+              }
+          );
+
+          sb.send(snd_bufs);
+        }
+      }
+  );
+  sc.send(snd_bufs);
+  ioc.run();
+  REQUIRE(caught_request);
+  REQUIRE(caught_reply);
+}
+
+TEST_CASE("DEALER to REP combination, synchronous", "[socket]") {
+  boost::asio::io_context ioc{};
+  auto const mq_name = subj(BOOST_CURRENT_FUNCTION);
+
+  auto sb = azmq::rep_socket{ioc};
+  sb.bind(mq_name);
+
+  auto sc = azmq::dealer_socket{ioc};
+  sc.connect(mq_name);
+
+// dealer request
+  sc.send("", ZMQ_SNDMORE); // emulate the envelope the REQ socket normal adds
+  sc.send(snd_bufs);
+  auto received_request = azmq::message_vector{};
+  sb.receive_more(received_request, 0);
+  REQUIRE(received_request.size() == 2);
+
+// reply
+  sb.send(snd_bufs);
+  auto received_reply = azmq::message_vector{};
+  sc.receive_more(received_reply, 0);
+  REQUIRE(received_reply.size() == 3);
+}
+
+TEST_CASE("REQ to ROUTER combination, synchronous", "[socket]") {
+  boost::asio::io_context ioc{};
+
+  auto const mq_name = subj(BOOST_CURRENT_FUNCTION);
+
+  auto sb = azmq::router_socket{ioc};
+  sb.bind(mq_name);
+
+  auto sc = azmq::req_socket{ioc};
+  auto const requester_id = std::string{"123abc"};
+  sc.set_option(azmq::socket::identity(requester_id));
+  sc.connect(mq_name);
+
+// Request
+  sc.send(snd_bufs);
+  auto received_request = azmq::message_vector{};
+  sb.receive_more(received_request, 0);
+  REQUIRE(received_request.size() == 4);
+  REQUIRE(received_request.at(0).string() == requester_id);
+  REQUIRE(received_request.at(1).string() == "");
+  REQUIRE(received_request.at(2) == snd_bufs.at(0));
+  REQUIRE(received_request.at(3) == snd_bufs.at(1));
+
+// Reply
+  auto const reply = azmq::message_vector{
+      received_request.at(0),
+      received_request.at(1),
+      azmq::message("foo"),
+      azmq::message("bar"),
+  };
+  sb.send(reply);
+
+  auto received_reply = azmq::message_vector{};
+  sc.receive_more(received_reply, 0);
+  REQUIRE(received_reply.size() == 2);
+  REQUIRE(received_reply.at(0).string() == std::string{"foo"});
+  REQUIRE(received_reply.at(1).string() == std::string{"bar"});
+}
+TEST_CASE("REQ to ROUTER combination, asynchronous", "[socket]") {
+  boost::asio::io_context ioc{};
+  auto sb = azmq::router_socket{ioc};
+  auto sc = azmq::req_socket{ioc};
+  bool caught_request = false;
+  bool caught_reply = false;
+
+  sb.bind(subj(BOOST_CURRENT_FUNCTION));
+  sc.connect(subj(BOOST_CURRENT_FUNCTION));
+  // catch the request
+  sb.async_receive(
+      [&](boost::system::error_code ec,
+          azmq::message &first_frame,
+          size_t bytes_transferred) {
+        REQUIRE(!ec);
+        auto message = azmq::message_vector{};
+        sb.receive_more(message, ZMQ_DONTWAIT);
+        REQUIRE(message.size() == 3);
+        REQUIRE(message.at(2) == snd_bufs.at(1));
+        caught_request = true;
+
+        // catch the reply
+        sc.async_receive(
+            [&sc,&caught_reply,&ioc](boost::system::error_code ec,
+                azmq::message &first_frame,
+                size_t bytes_transferred) {
+              SCOPE_EXIT { ioc.stop(); };
+              REQUIRE(!ec);
+              auto received_reply = azmq::message_vector{};
+              sc.receive_more(received_reply, ZMQ_DONTWAIT);
+              REQUIRE(received_reply.size() == 1);
+              caught_reply = true;
+            }
+        );
+
+        sb.send(first_frame,ZMQ_SNDMORE);
+        sb.send(message);
+      }
+  );
+  sc.send(snd_bufs);
+  ioc.run();
+  REQUIRE(caught_request);
+  REQUIRE(caught_reply);
+}
+
+TEST_CASE("DEALER to ROUTER combination, synchronous", "[socket]") {
+  boost::asio::io_context ioc{};
+
+  auto const mq_name = subj(BOOST_CURRENT_FUNCTION);
+
+  auto sb = azmq::router_socket{ioc};
+  sb.bind(mq_name);
+
+  auto sc = azmq::dealer_socket{ioc};
+  auto const requester_id = std::string{"async_requester"};
+  sc.set_option(azmq::socket::identity(requester_id));
+  sc.connect(mq_name);
+
+// Request
+  sc.send("", ZMQ_SNDMORE); // emulate the envelope the REQ socket normal adds
+  sc.send(snd_bufs);
+
+  auto received_request = azmq::message_vector{};
+  sb.receive_more(received_request, 0);
+  REQUIRE(received_request.size() == 4);
+  REQUIRE(received_request.at(0).string() == requester_id);
+  REQUIRE(received_request.at(1).string() == "");
+  REQUIRE(received_request.at(2) == snd_bufs.at(0));
+  REQUIRE(received_request.at(3) == snd_bufs.at(1));
+
+// Reply
+  auto const reply = azmq::message_vector{
+      received_request.at(0),
+      received_request.at(1),
+      azmq::message("foo"),
+      azmq::message("bar"),
+  };
+  sb.send(reply);
+
+  auto received_reply = azmq::message_vector{};
+  sc.receive_more(received_reply, 0);
+  REQUIRE(received_reply.size() == 3);
+  REQUIRE(received_reply.at(0).string() == std::string{""});
+  REQUIRE(received_reply.at(1).string() == std::string{"foo"});
+  REQUIRE(received_reply.at(2).string() == std::string{"bar"});
+}
---
+++ test/socket/shared_queue.cpp
@@ -0,0 +1,118 @@
+/*
+    Copyright (c) 2013-2014 Contributors as noted in the AUTHORS file
+
+    This file is part of azmq
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#include <azmq/socket.hpp>
+#include <boost/asio/buffer.hpp>
+#include <catch2/catch.hpp>
+#include <array>
+#include <string>
+#include <thread>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/atomic/atomic_flag.hpp>
+#include <iostream>
+
+namespace {
+
+std::array<boost::asio::const_buffer, 2> const snd_bufs = {{
+                                                               boost::asio::buffer("A"),
+                                                               boost::asio::buffer("B")
+                                                           }};
+
+auto CopyMore(azmq::message const &source_first_frame,
+          azmq::socket &source,
+          azmq::socket &destination
+          ) ->void {
+  auto more = source_first_frame.more();
+  destination.send(source_first_frame, more ? ZMQ_SNDMORE : 0);
+  while (more) {
+    auto next_msg = azmq::message{};
+    source.receive(next_msg);
+    more = next_msg.more();
+    destination.send(next_msg, more ? ZMQ_SNDMORE : 0);
+  }
+};
+
+auto AsyncProxy( azmq::socket &source, azmq::socket &destination ) -> void{
+  source.async_receive([&](boost::system::error_code ec,
+                           azmq::message &first_frame,
+                           size_t /*bytes_transferred*/) {
+    if (!ec) {
+      CopyMore(first_frame, source, destination);
+      AsyncProxy( source, destination);
+    }
+  });
+};
+
+}
+
+TEST_CASE("Shared queue", "[socket]") {
+
+  auto rrclient = [](std::string const &queue_name) {
+    boost::asio::io_context ioc{};
+    auto requester = azmq::req_socket{ioc};
+    requester.connect(queue_name);
+    for (int request_num = 0; request_num < 10; request_num++) {
+      [[maybe_unused]] auto byte_sent = requester.send(boost::asio::buffer("Hello"));
+      azmq::message_vector reply{};
+      requester.receive_more(reply, 0);
+    }
+    [[maybe_unused]] auto byte_sent = requester.send(boost::asio::buffer("done"));
+  };
+
+  auto rrworker = [](std::string const &queue_name) {
+    boost::asio::io_context ioc{};
+    auto rep = azmq::rep_socket{ioc};
+    rep.connect(queue_name);
+    while (1) {
+      azmq::message_vector request{};
+      rep.receive_more(request, 0);
+      if (request.size() == 1 && request.at(0) == boost::asio::buffer("done")) {
+        break;
+      } else {
+        auto timer = boost::asio::steady_timer(ioc);
+        timer.expires_after(std::chrono::milliseconds(20));
+        timer.wait();
+        rep.send("work done");
+      }
+    }
+  };
+
+  auto rrbroker = [](std::string const& front_queue_name, std::string const & back_queue_name,
+                     boost::asio::io_context &ioc) {
+
+    auto frontend = azmq::router_socket{ioc};
+    frontend.bind(front_queue_name);
+
+    auto backend = azmq::dealer_socket{ioc};
+    backend.bind(back_queue_name);
+
+    AsyncProxy( frontend, backend );
+    AsyncProxy( backend, frontend);
+
+    ioc.run();
+  };
+
+  auto const backend_queue_name = std::string("inproc://backend_");
+  auto const frontend_queue_name = std::string("inproc://tmp/frontend_");
+
+  boost::asio::io_context broker_ioc{};
+  auto broker = std::thread([&] { rrbroker(frontend_queue_name, backend_queue_name,broker_ioc); });
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  auto worker1 = std::thread([&] { rrworker(backend_queue_name); });
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  auto client1 = std::thread([&] { rrclient(frontend_queue_name); });
+
+  client1.join();
+  worker1.join();
+
+  broker_ioc.stop();
+  broker.join();
+
+}
